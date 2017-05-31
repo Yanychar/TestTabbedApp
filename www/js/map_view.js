b54c1409df1afed0b3f590d04eb207791f85ecb8
Yanychar <serguei.sevastianov@gmail.com>
2017-05-31 18:10:28 +0300

    <temp

--- www/js/map_view.js
+++ www/js/map_view.js
@@ -17,19 +17,6 @@ var mapManipulator = {
 
                 mapManipulator.myMap = mapManipulator.drawMap( mapManipulator.currLoc );
 
-/*                mapManipulator.drawMarker( 
-                                mapManipulator.myMap,
-                                mapManipulator.getMarker( "#a82bdd", "5", "Тест тоолтип" ), 
-                                mapManipulator.currLoc );
-                                
-                                
-*/
-                getSpots( 
-                    pos.coords.latitude, 
-                    pos.coords.longitude,
-                    mapManipulator.getSpotsSuccess,
-                    mapManipulator.getSpotsFailed
-                );
                           
                 
             };
@@ -42,8 +29,6 @@ var mapManipulator = {
                           }};
                 success( pos );              
                 
-                
-//                mapManipulator.drawMap( mapManipulator.myMap );
             };
 
             // Find the users current position.  Cache the location for 5 minutes, timeout after 6 seconds
@@ -79,95 +64,5 @@ var mapManipulator = {
         
         return map;
     },
-    
-
-    createMarker:  function( spotDescr ) {
-
-        var gMarker = new google.maps.Marker(
-            {
-                icon:   {
-                    path: "M24 0c-8.284 0-15 6.716-15 15 0 15 15 33 15 33s15-18 15-33c0-8.284-6.716-15-15-15zM24 24c-4.971 0-9-4.029-9-9s4.029-9 9-9 9 4.029 9 9-4.029 9-9 9z",
-                    anchor: {x: 24, y: 48},
-                    fillColor: defaultIfBlank( spotDescr.getSpotColor(), "#a82bdd" ),
-                    fillOpacity: 1,
-                    //    scale: 1,
-                    strokeColor: "black",
-                    strokeWeight: 2,
-                    labelOrigin: {x: 24, y: 15}
-                },
-                shape:  {
-                    coords: [1, 1, 1, 48, 48, 48, 48, 1],
-                    type: 'poly'
-                },
-                title:  defaultIfBlank( spotDescr.name, "" ),
-                label:  {
-                    text: defaultIfBlank( String( spotDescr.rating ), "" ),
-                    color: defaultIfBlank( spotDescr.getSpotColor(), "#a82bdd" ),
-                    fontWeight: "bold"
-                },
-            }
-        );
-        
-        gMarker.customData = spotDescr;
-        
-    
-        return gMarker;
-    },
-    
-    drawMarker: function ( map, gMarker, latlng ) {
-        gMarker.setPosition( latlng );
-        gMarker.setMap( map );
-        
-        console.log( "DRAW MARKER: " + gMarker.customData.name + ", " + gMarker.customData.owner.getFullFNfirst() ); 
-    
-    },
-
-    createAndDrawMarker: function ( map, spotDescr ) {
-        var gMarker = mapManipulator.createMarker( spotDescr );
-        mapManipulator.drawMarker(  map, 
-                                    gMarker,
-                                    spotDescr.getLatLng()
-        );
-        
-        return gMarker;
-    },
-
-    getSpotsSuccess: function( spList ) {
-        console.log( "Spots were read: " );
-        var loc;
-        var gm;
-        for ( var i = 0; i < spList.length; i++ ) {
-            console.log( "  " + spList[i].name + ": (" + spList[i].rating + ")"
-                        +"  " + "Pos( " + spList[i].pos.lat + ", " + spList[i].pos.lng + " )"           
-            ); 
-
-            gm = mapManipulator.createAndDrawMarker( mapManipulator.myMap, 
-                    spList[ i ]
-            );
-
-            google.maps.event.addListener( gm, 'mouseover', function( event ){
-                
-                console.log( "WILL BE SHOWN: " + this.title + ", " + this.customData.owner.getFullFNfirst()); 
-
-                spotWindow.show( this );
-                
-
-            });            
-/*            
-            google.maps.event.addListener( gm, 'click', function( event ){
-
-                if ( event.Ba.isTrusted ){
-                    console.log( "Marker clicked: " + this.title );
-                }
-
-            });            
-*/            
-        }
-    },
-
-    getSpotsFailed: function() {
-        console.log( "Failed to read Spots list" );
-    }
-
     
 }
