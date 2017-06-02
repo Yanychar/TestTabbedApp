561e53a98f72c7d1a1d9d6ebe494c7046fdc9b02
Yanychar <serguei.sevastianov@gmail.com>
2017-06-02 16:11:02 +0300

    Tabs and 'pages' (still one page) ready. Tab 'tap' is handled properly
    Icons shall be added

--- www/js/init.js
+++ www/js/init.js
@@ -12,47 +12,54 @@ function initApplication() {
 
     document.addEventListener( "deviceready", this.onDeviceReady, false);
     
+    $(document).on("pagecreate", "#ViewPage", function() {
+
+        console.log("**** Page Create ***");
+
+        setupNavbar();
+        
+        $("#tab_bar a").on("tap", function() {
+                console.log( "Tap tab: " + $(this).text());
+
+                        activatePage( $(this) );
+
+        });
+
+        activatePage( $("[data-role=navbar] ul").children().first());
+        
+
+        $("#tab_bar a").on("tap", function() {
+            console.log( "Tap tab: " + $(this).text());
+          activeTab = $(this).text();
+
+          if ( activeTab == "Map" ) {
+              $( "#map_panel" ).show();
+              $( "#addon_panel" ).hide();
+          } else if ( activeTab == "Spots" ) {
+              $( "#map_panel" ).hide();
+              $( "#addon_panel" ).show();
+          } else {
+              $( "#map_panel" ).hide();
+              $( "#addon_panel" ).show();
+          }
+        });
+
+        var ft = $("[data-role=navbar] ul").children().first();
+        if ( ft != null ) {
+            ft.tap();
+        } else {
+            console.error( "First tab was not found!" );
+        }
+
+
+    });
+    
+    
 }
 
 function onDeviceReady() {
 	console.log( "onDeviceReady ... " );
-
-    
 }
-
-
-
-$(document).on("pagecreate", "#ViewPage", function() {
-    console.log("**** Page Create ***");
-
-    setupNavbar();
-
-    $("#tab_bar a").on("tap", function() {
-        console.log( "Tap tab: " + $(this).text());
-      activeTab = $(this).text();
-
-      if ( activeTab == "Map" ) {
-          $( "#map_panel" ).show();
-          $( "#addon_panel" ).hide();
-      } else if ( activeTab == "Spots" ) {
-          $( "#map_panel" ).hide();
-          $( "#addon_panel" ).show();
-      } else {
-          $( "#map_panel" ).hide();
-          $( "#addon_panel" ).show();
-      }
-    });
-
-    var ft = $("[data-role=navbar] ul").children().first();
-    if ( ft != null ) {
-        ft.tap();
-    } else {
-        console.error( "First tab was not found!" );
-    }
-    
-    
-});
-
     
 
 function setupNavbar( activateTab ) {
@@ -71,4 +78,34 @@ function setupNavbar( activateTab ) {
     
 }
 
+function activatePage( clickedTab ) {
+	console.log( "Activated tab No.: " + clickedTab.attr( "href" ));
+//	console.log( "Firsttab : " + $("[data-role=navbar] ul").children().first().text());
+
+	$("[data-role=main]").children().each( function() {
+        $( this ).hide();
+    });
+
+    switch ( clickedTab.attr( "href" )) {
+        case "#mapview":
+            $( "#mapview" ).show();
+            break;
+        case "#spotsview":
+            $( "#spotsview" ).show();
+            break;
+        case "#commentsview":
+            $( "#commentsview" ).show();
+            break;
+        case "#messagesview":
+            $( "#messagesview" ).show();
+            break;
+        case "#profileview":
+            $( "#profileview" ).show();
+            break;
+        default:
+            console.log( "Wrong tab name specified!" );
+          break;
+	}
+	
+}
 
