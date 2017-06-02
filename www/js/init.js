d954618d8ebe53d53caeda0d95d02de3af655f12
Yanychar <serguei.sevastianov@gmail.com>
2017-06-02 16:22:20 +0300

    Small bug in prev.

--- www/js/init.js
+++ www/js/init.js
@@ -28,28 +28,6 @@ function initApplication() {
         activatePage( $("[data-role=navbar] ul").children().first());
         
 
-        $("#tab_bar a").on("tap", function() {
-            console.log( "Tap tab: " + $(this).text());
-          activeTab = $(this).text();
-
-          if ( activeTab == "Map" ) {
-              $( "#map_panel" ).show();
-              $( "#addon_panel" ).hide();
-          } else if ( activeTab == "Spots" ) {
-              $( "#map_panel" ).hide();
-              $( "#addon_panel" ).show();
-          } else {
-              $( "#map_panel" ).hide();
-              $( "#addon_panel" ).show();
-          }
-        });
-
-        var ft = $("[data-role=navbar] ul").children().first();
-        if ( ft != null ) {
-            ft.tap();
-        } else {
-            console.error( "First tab was not found!" );
-        }
 
 
     });
